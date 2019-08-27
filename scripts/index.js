19fcab159355c181882b34223350d250871dd364
Simms, Alsahid A <alsahid.a.simms@accenture.com>
2019-08-27 19:46:33 +0100

    firebase authentication - need to modify

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,27 @@
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.getElementById('user_div').style.display = "block";
+    document.getElementById('login_div').style.display = "none";
+
+  } else {
+    // No user is signed in.
+    document.getElementById('user_div').style.display = "none";
+    document.getElementById('login_div').style.display = "block";
+
+
+  }
+});
+
 function login() {
 
-  var email = document.getElementById('email');
-  var password = document.getElementById('password');
+  var email = document.getElementById('email_field');
+  var password = document.getElementById('password_field');
 
-
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
 }
